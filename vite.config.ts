e0a4dd689a2df2e0e90105ebe9df98d5384e5369
Kaue-TecsaGroup <kaue.melo@tecsagroup.com.br>
2026-05-05 08:49:13 -0300

    feat: Adding hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const hmrClientPort = Number(process.env.VITE_HMR_CLIENT_PORT ?? 5173)
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    hmr: {
+      clientPort: hmrClientPort
+    },
+    watch: {
+      usePolling: true,
+      interval: 100
+    }
   }
 })
